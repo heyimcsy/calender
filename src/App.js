44a504001d64669f2691da9784402dbc1f3a5403
최서윤 <cinnamon1995@naver.com>
2023-04-09 22:27:42 +0900

    calener with library

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import Calendar from 'react-calendar'
+import moment from 'moment/moment'
+import '../src/itCalender.css'
 
 function App() {
+  const [value, onChange] = useState(new Date())
+  console.log('onChange', Calendar)
+  const marks = ['15-04-2023', '20-04-2023']
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      캘린더 연습
+      <Calendar onChange={onChange} value={value} />
+      <Calendar
+        onChange={onChange}
+        value={value}
+        locale="en-EN"
+        tileClassName={({ date, view }) => {
+          if (marks.find((x) => x === moment(date).format('DD-MM-YYYY'))) {
+            return 'highlight'
+          }
+        }}
+      />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
